a6c479ea3ed79f96e0c48949c8609002c25b24f1
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-03-14 17:13:16 +0800

    feat: updet

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,12 @@
 export default {
-    logo: <span>My Nextra Documentation</span>,
+    logo:
+        <div className={'flex hover:nx-opacity-75 items-center'} onClick={()=>{
+            window.location.href = '/'
+        }}>
+        <img src="/logo.svg" style={{width:'32px'}} alt=""/>
+        <span className={'mx-2 font-extrabold hidden md:inline select-none'}>CANYON</span>
+        <span className={'text-gray-600 font-normal hidden lg:!inline whitespace-no-wrap'}>JavaScript code coverage solution</span>
+    </div>,
     project: {
         link: 'https://github.com/shuding/nextra'
     }
