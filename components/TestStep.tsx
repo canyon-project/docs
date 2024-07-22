be95d815eaff90fdd62398524e2cd284ced8c4c7
Allen Zhang <37892968+zhangtao25@users.noreply.github.com>
2024-07-22 23:46:23 +0800

    upd

--- components/TestStep.tsx
+++ components/TestStep.tsx
@@ -1,38 +1,52 @@
 import { Button, Steps } from "antd";
 import { AlertOutlined } from "@ant-design/icons";
+const IconWrapper = ({ children }) => {
+    return <div className={'bg-gray-100 rounded-lg flex items-center justify-center h-[28px] w-[28px] mt-1'}>
+        {children}
+    </div>
+}
+
+// <AlertOutlined style={{fontSize: '14px'}} className={'gray-800'}/>
 
 const TestStep = () => {
-  return (
-    <div>
-      <Button type={"primary"}>你好</Button>
-      <Steps
-        direction="vertical"
-        current={undefined}
-        items={[
-          {
-            title: "Open-source code",
-            description:
+    return (
+        <div className={'ml-3.5 mt-10 mb-6'}>
+            <Steps
+                direction="vertical"
+                current={null}
+                items={[
+                    {
+                        title: "Open-source code",
+                        description:
               "Source code is open and auditable. Built with privacy and security in mind.",
-            icon: <AlertOutlined />,
+                        icon: <IconWrapper>
+                            <AlertOutlined style={{fontSize: '14px'}} className={'gray-800'}/>
+                        </IconWrapper>,
           },
-          {
-            title: "Cross-platform apps",
-            description:
-              "Works on Web, Mac, Windows, Linux, and CLI. No installation is required.",
-            icon: <AlertOutlined />,
-          },
-          {
-            title: "Open-source code",
-            description:
-              "Source code is open and auditable. Built with privacy and security in mind.",
-            icon: <AlertOutlined />,
-          },
-          {
-            title: "Cross-platform apps",
-            description:
-              "Works on Web, Mac, Windows, Linux, and CLI. No installation is required.",
-            icon: <AlertOutlined />,
-          },
+                    {
+                        title: "Open-source code",
+                        description:
+                            "Source code is open and auditable. Built with privacy and security in mind.",
+                        icon: <IconWrapper>
+                            <AlertOutlined style={{fontSize: '14px'}} className={'gray-800'}/>
+                        </IconWrapper>,
+                    },
+                    {
+                        title: "Open-source code",
+                        description:
+                            "Source code is open and auditable. Built with privacy and security in mind.",
+                        icon: <IconWrapper>
+                            <AlertOutlined style={{fontSize: '14px'}} className={'gray-800'}/>
+                        </IconWrapper>,
+                    },
+                    {
+                        title: "Open-source code",
+                        description:
+                            "Source code is open and auditable. Built with privacy and security in mind.",
+                        icon: <IconWrapper>
+                            <AlertOutlined style={{fontSize: '14px'}} className={'gray-800'}/>
+                        </IconWrapper>,
+                    },
         ]}
       ></Steps>
     </div>
