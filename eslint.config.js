aa58bb4b4f7a01c591bf5033f303127defc9a27a
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-08-29 17:43:26 +0800

    upd

---
+++ eslint.config.js
@@ -0,0 +1,25 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  ignores: [
+    'public',
+    'build',
+    'dist',
+    'node_modules',
+    'coverage',
+    'src/assets/**',
+  ],
+  stylistic: {
+  },
+  jsonc: true,
+  formatters: {
+    markdown: true,
+  },
+  typescript: true,
+  react: true,
+  markdown: true,
+  extends: [
+    'next/core-web-vitals',
+  ],
+  rules: {},
+})
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss'
+
+const config = {
+  darkMode: ['class', '[class~=\'dark\']'],
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/**/*.{js,ts,jsx,tsx,mdx}',
+    './theme.config.tsx',
+  ],
+  prefix: '',
+  theme: {
+  },
+  plugins: [
+  ],
+} satisfies Config
+
+export default config
