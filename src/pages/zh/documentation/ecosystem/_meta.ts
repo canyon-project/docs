5f52a8d860b58d53ae8ddb3742bb52b114812413
Allen Zhang (张涛) <wr_zhang25@163.com>
2024-09-03 11:28:42 +0800

    upd

--- src/pages/zh/documentation/ecosystem/_meta.ts
+++ src/pages/zh/documentation/ecosystem/_meta.ts
@@ -1,5 +1,6 @@
 export default {
   "babel-plugin-istanbul": "babel-plugin-istanbul",
+  "vite-plugin-istanbul": "vite-plugin-istanbul",
   uploader: "Uploader",
   "chrome-extension": "Chrome Extension",
 };
