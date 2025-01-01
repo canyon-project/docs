f0f1099370762dd24442d54397a7c88234ae834b
zhangtao25 <wr.zhang25@gmail.com>
2025-01-01 21:33:09 +0800

    chore: daily development

--- next.config.mjs
+++ next.config.mjs
@@ -4,15 +4,16 @@ import nextra from 'nextra'
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  // output: 'export',
-  // images: {
-  //   unoptimized: true // mandatory, otherwise won't export
-  // },
-  // distDir: "dist"
+  output: 'export',
+  images: {
+    unoptimized: true // mandatory, otherwise won't export
+  },
+  distDir: "dist"
 }
 const withNextra = nextra({
   theme: 'nextra-theme-docs',
-  themeConfig: './theme.config.jsx'
+  themeConfig: './theme.config.jsx',
+  latex: true,
 })
 
 export default withNextra(nextConfig)
