47af5325f1c56dac0bd80b46a4c4d67356dc98a9
zhangtao25 <wr.zhang25@gmail.com>
2025-01-02 16:45:32 +0800

    chore: daily development

--- theme.config.jsx
+++ theme.config.jsx
@@ -1,5 +1,21 @@
+const t = (key) => key;
 export default {
-  logo: <span>Canyon</span>,
+  logo: () => (
+    <div
+      className="flex hover:nx-opacity-75 items-center"
+      onClick={() => {
+        window.location.href = "/";
+      }}
+    >
+      <img src="/logo.svg" style={{ width: "32px" }} alt="" />
+      <div className="mx-2 font-extrabold hidden md:inline select-none">
+        CANYON
+      </div>
+      <div className="text-gray-600 font-normal hidden lg:!inline whitespace-no-wrap">
+        JavaScript code coverage solution
+      </div>
+    </div>
+  ),
   project: {
     link: 'https://github.com/canyon-project/canyon'
   }
