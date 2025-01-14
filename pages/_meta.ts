51c5dfbfc0feb7f96b9d287305ce4f0154025817
zhangtao25 <wr.zhang25@gmail.com>
2025-01-14 11:46:05 +0800

    chore: daily development

--- pages/_meta.ts
+++ pages/_meta.ts
@@ -2,6 +2,10 @@ export default {
   index: {
     type: "page",
     display: "hidden",
+    "title": "Introduction",
+    // "type": "page",
+    "hidden": true,
+    // "display": "hidden"
   },
   documentation: {
     title: "Documentation",
